bf9ce54d3e5b00310d57c626f0f368b6ff0fd626
ginanjarn <ginanjarn@gmail.com>
2023-02-08 12:44:53 +0700

    improve diagnostic module portability

--- pyserver/services/diagnostics.py
+++ pyserver/services/diagnostics.py
@@ -1,11 +1,10 @@
 """completion service"""
 
 import re
-from collections import namedtuple
 from dataclasses import dataclass
 from io import StringIO
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, Iterator
 
 from pyflakes import api as pyflakes_api
 from pyflakes.reporter import Reporter
@@ -21,12 +20,49 @@ class DiagnosticParams:
     version: int
 
 
-Report = namedtuple("Reporte", ["warning", "error"])
+KIND_ERROR = 1
+KIND_WARNING = 2
 
-# <file_name>:<line>:<column> <message>
-warning_regex = re.compile(r"^.+:(\d+):\d+\ (.+)")
-# <file_name>:<line>:<column>: <message>
-error_regex = re.compile(r".+:(\d+):\d+:\ (.+)")
+PYFLAKES_REGEX = {
+    # <file_name>:<line>:<column>: <message>
+    KIND_ERROR: re.compile(r".+:(\d+):\d+:\ (.+)"),
+    # <file_name>:<line>:<column> <message>
+    KIND_WARNING: re.compile(r"^.+:(\d+):\d+\ (.+)"),
+}
+
+
+@dataclass
+class ReportItem:
+    source: str
+    severity_kind: int
+    lineno: int
+    message: str
+
+
+@dataclass
+class Report:
+    source: str
+    error: str
+    warning: str
+
+    def _parse_report(self, kind: int):
+        report = {KIND_ERROR: self.error, KIND_WARNING: self.warning}[kind]
+        # pyflakes report pattern
+        regex = PYFLAKES_REGEX[kind]
+
+        for line in report.splitlines():
+            if match := regex.match(line):
+                lineno, message = match.groups()
+                # use zero based line index
+                lineno = int(lineno) - 1
+                yield ReportItem(self.source, kind, int(lineno), message)
+
+    def get_items(self) -> Iterator[ReportItem]:
+        if self.error:
+            yield from self._parse_report(KIND_ERROR)
+
+        elif self.warning:
+            yield from self._parse_report(KIND_WARNING)
 
 
 class DiagnosticService(Services):
@@ -34,48 +70,36 @@ class DiagnosticService(Services):
         self.params = params
 
     def execute(self) -> Report:
-        warning_stream = StringIO()
-        error_stream = StringIO()
-        reporter = Reporter(warning_stream, error_stream)
 
+        # get pyflakes report
+        warning_buffer = StringIO()
+        error_buffer = StringIO()
+
+        reporter = Reporter(warning_buffer, error_buffer)
         pyflakes_api.check(self.params.text, str(self.params.file_path), reporter)
 
-        return Report(warning_stream.getvalue(), error_stream.getvalue())
+        return Report(
+            "pyflakes", warning=warning_buffer.getvalue(), error=error_buffer.getvalue()
+        )
 
-    def build_items(self, report: Report):
+    def build_items(self, report: Report) -> Iterator[Dict[str, Any]]:
         lines = self.params.text.split("\n")
 
-        def build_line_item(pattern, line, severity):
-            match = pattern.match(line)
-            if not match:
-                return None
-
-            line_index = int(match.group(1)) - 1  # pyflakes use 1-based line index
-            message = match.group(2)
-
+        def build_line_item(item: ReportItem):
             return {
                 # unable determine error location correctly
                 # put the line as range
                 "range": {
-                    "start": {"line": line_index, "character": 0},
-                    "end": {"line": line_index, "character": len(lines[line_index])},
+                    "start": {"line": item.lineno, "character": 0},
+                    "end": {"line": item.lineno, "character": len(lines[item.lineno])},
                 },
-                "severity": severity,
-                "source": "pyflakes",
-                "message": message,
+                "severity": item.severity_kind,
+                "source": item.source,
+                "message": item.message,
             }
 
-        if report.error:
-            error_lines = report.error.splitlines()
-            # pyflakes only report first error occured while checking
-            if item := build_line_item(error_regex, error_lines[0], 1):
-                yield item
-
-        elif report.warning:
-            warning_lines = report.warning.splitlines()
-            for w_line in warning_lines:
-                if item := build_line_item(warning_regex, w_line, 2):
-                    yield item
+        for item in report.get_items():
+            yield build_line_item(item)
 
     def get_result(self) -> Dict[str, Any]:
         report = self.execute()
