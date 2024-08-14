a208c8bf48b59f713aef46d2f363465dcaf653db
ginanjarn <ginanjarn@gmail.com>
2024-08-14 16:37:57 +0700

    better name for get text changes

--- pyserver/services/diffutils.py
+++ pyserver/services/diffutils.py
@@ -4,46 +4,54 @@ import difflib
 from typing import Iterator, List
 
 
-def _get_text_changes(a: str, b: str, n: int = 3) -> Iterator:
-    """get text change from a to b with added n delta"""
+def _get_text_changes(old: str, new: str, group_size: int = 3) -> Iterator[dict]:
+    """get text changes"""
 
-    # SequenceMatcher requires a and b to be a list
-    a = a.split("\n")
-    b = b.split("\n")
+    line_separator = "\n"
 
-    for group in difflib.SequenceMatcher(None, a, b).get_grouped_opcodes(n):
+    # SequenceMatcher requires old and new to be a list
+    old = old.split(line_separator)
+    new = new.split(line_separator)
+
+    # See implementation reference in 'difflib.unified_diff'
+    for group in difflib.SequenceMatcher(None, old, new).get_grouped_opcodes(
+        group_size
+    ):
         first, last = group[0], group[-1]
-        start_line_rm, end_line_rm = first[1], last[2] - 1
+        start_removed_line_index, end_remove_line_index = first[1], last[2] - 1
 
-        rm_buffer = []
-        add_buffer = []
+        removed_lines = []
+        add_lines = []
 
         for tag, i1, i2, j1, j2 in group:
             if tag == "equal":
-                for line in a[i1:i2]:
-                    rm_buffer.append(line)
-                    add_buffer.append(line)
+                for line in old[i1:i2]:
+                    removed_lines.append(line)
+                    add_lines.append(line)
                 continue
             if tag in {"replace", "delete"}:
-                for line in a[i1:i2]:
-                    rm_buffer.append(line)
+                for line in old[i1:i2]:
+                    removed_lines.append(line)
             if tag in {"replace", "insert"}:
-                for line in b[j1:j2]:
-                    add_buffer.append(line)
+                for line in new[j1:j2]:
+                    add_lines.append(line)
 
-        rm_text = "\n".join(rm_buffer)
-        add_text = "\n".join(add_buffer)
+        removed_text = line_separator.join(removed_lines)
+        added_text = line_separator.join(add_lines)
 
         yield {
             "range": {
-                "start": {"line": start_line_rm, "character": 0},
-                "end": {"line": end_line_rm, "character": len(rm_buffer[-1])},
+                "start": {"line": start_removed_line_index, "character": 0},
+                "end": {
+                    "line": end_remove_line_index,
+                    "character": len(removed_lines[-1]),
+                },
             },
-            "newText": add_text,
-            "rangeLength": len(rm_text),
+            "newText": added_text,
+            "rangeLength": len(removed_text),
         }
 
 
-def get_text_changes(src: str, new: str) -> List[dict]:
-    """get text changes from srctext to newtext"""
-    return list(_get_text_changes(src, new))
+def get_text_changes(old: str, new: str) -> List[dict]:
+    """get text changes"""
+    return list(_get_text_changes(old, new, group_size=1))
