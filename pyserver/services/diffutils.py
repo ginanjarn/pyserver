fef9232355bad2dcb4031d7a2422b2aa0a15b211
ginanjarn <ginanjarn@gmail.com>
2024-10-15 17:18:40 +0700

    improve to tangible name

--- pyserver/services/diffutils.py
+++ pyserver/services/diffutils.py
@@ -4,7 +4,7 @@ import difflib
 from typing import Iterator, List
 
 
-def _get_text_changes(old: str, new: str, group_size: int = 3) -> Iterator[dict]:
+def _get_text_changes(old: str, new: str, delta: int = 3) -> Iterator[dict]:
     """get text changes"""
 
     line_separator = "\n"
@@ -14,44 +14,42 @@ def _get_text_changes(old: str, new: str, group_size: int = 3) -> Iterator[dict]
     new = new.split(line_separator)
 
     # See implementation reference in 'difflib.unified_diff'
-    for group in difflib.SequenceMatcher(None, old, new).get_grouped_opcodes(
-        group_size
-    ):
-        first, last = group[0], group[-1]
-        start_removed_line_index, end_remove_line_index = first[1], last[2] - 1
+    for groups in difflib.SequenceMatcher(None, old, new).get_grouped_opcodes(delta):
+        first_group, last_group = groups[0], groups[-1]
+        start_removed_line, end_remove_line = (first_group[1], last_group[2] - 1)
 
         removed_lines = []
-        add_lines = []
+        insert_lines = []
 
-        for tag, i1, i2, j1, j2 in group:
+        for tag, i1, i2, j1, j2 in groups:
             if tag == "equal":
                 for line in old[i1:i2]:
                     removed_lines.append(line)
-                    add_lines.append(line)
+                    insert_lines.append(line)
                 continue
             if tag in {"replace", "delete"}:
                 for line in old[i1:i2]:
                     removed_lines.append(line)
             if tag in {"replace", "insert"}:
                 for line in new[j1:j2]:
-                    add_lines.append(line)
+                    insert_lines.append(line)
 
         removed_text = line_separator.join(removed_lines)
-        added_text = line_separator.join(add_lines)
+        insert_text = line_separator.join(insert_lines)
 
         yield {
             "range": {
-                "start": {"line": start_removed_line_index, "character": 0},
+                "start": {"line": start_removed_line, "character": 0},
                 "end": {
-                    "line": end_remove_line_index,
+                    "line": end_remove_line,
                     "character": len(removed_lines[-1]),
                 },
             },
-            "newText": added_text,
+            "newText": insert_text,
             "rangeLength": len(removed_text),
         }
 
 
 def get_text_changes(old: str, new: str) -> List[dict]:
     """get text changes"""
-    return list(_get_text_changes(old, new, group_size=1))
+    return list(_get_text_changes(old, new, delta=1))
