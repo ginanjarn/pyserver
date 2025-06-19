c1bc101c40cb281301401b53f26e8676adce40ea
ginanjarn <ginanjarn@gmail.com>
2025-06-19 15:52:30 +0700

    organize offset calculator

--- pyserver/document.py
+++ pyserver/document.py
@@ -23,9 +23,23 @@ class Document:
 LineCharacter = namedtuple("LineCharacter", ["line", "character"])
 
 
-def _get_offset(lines: List[str], row: int, column: int) -> int:
-    line_offset = sum([len(l) for l in lines[:row]])
-    return line_offset + column
+class OffsetCalculator:
+    def __init__(self, text: str) -> None:
+        self.lines = text.splitlines(keepends=True)
+        self.text_len = len(text)
+
+    def get_offset(self, row: int, column: int) -> int:
+        line_offset = sum([len(l) for l in self.lines[:row]])
+        return line_offset + column
+
+    def get_rowcolumn(self, offset: int) -> tuple[int, int]:
+        calculated_len = 0
+        for index, line in enumerate(self.lines):
+            if (temp := calculated_len + len(line)) and temp < offset:
+                calculated_len = temp
+                continue
+            return index, offset - calculated_len
+        return 0, 0
 
 
 def _update_text(old_text: str, changes: List[dict]) -> str:
@@ -40,9 +54,10 @@ def _update_text(old_text: str, changes: List[dict]) -> str:
         except KeyError as err:
             raise errors.InvalidParams(f"invalid params {err}") from err
 
-        lines = temp.splitlines(keepends=True)
-        start_offset = _get_offset(lines, start.line, start.character)
-        end_offset = _get_offset(lines, end.line, end.character)
+        calculator = OffsetCalculator(temp)
+        start_offset = calculator.get_offset(*start)
+        end_offset = calculator.get_offset(*end)
+
         temp = f"{temp[:start_offset]}{new_text}{temp[end_offset:]}"
 
     return temp
