fbbbb3d1af54c62ba20537a6ede461f6fea3da8c
ginanjarn <ginanjarn@gmail.com>
2025-09-21 21:59:55 +0700

    improve Error classes

--- pyserver/errors.py
+++ pyserver/errors.py
@@ -3,97 +3,133 @@
 from typing import Union, Dict, Any, Optional
 
 
-class ContentIncomplete(ValueError):
-    """expected size less than defined"""
+class JSONRPCException(Exception):
+    """Base json-rpc exception"""
+
+    code: int
+    message: str
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
 
 
-class RPCException(Exception):
-    """base rpc exception"""
-
-    code = 0
-    message = ""
-
-
-class ParseError(RPCException):
-    """message not comply to jsonrpc 2.0 specification"""
+class ParseError(JSONRPCException):
+    """An error occurred while parsing the JSON text"""
 
     code = -32700
     message = "parse error"
 
 
-class InvalidRequest(RPCException):
-    """invalid request"""
+class InvalidRequest(JSONRPCException):
+    """The JSON sent is not a valid Request object"""
 
     code = -32600
     message = "invalid request"
 
 
-class MethodNotFound(RPCException):
-    """method not found"""
+class MethodNotFound(JSONRPCException):
+    """The method does not exist / is not available"""
 
     code = -32601
     message = "method not found"
 
 
-class InvalidParams(RPCException):
-    """invalid params"""
+class InvalidParams(JSONRPCException):
+    """Invalid method parameter(s)"""
 
     code = -32602
     message = "invalid params"
 
 
-class InternalError(RPCException):
-    """internal error"""
+class InternalError(JSONRPCException):
+    """Internal JSON-RPC error"""
 
     code = -32603
     message = "internal error"
 
 
-class ServerNotInitialized(RPCException):
-    """workspace not initialize"""
+class ServerNotInitialized(JSONRPCException):
+    """Server received a notification or request before the server received the `initialize` request"""
 
     code = -32002
     message = "server not initialized"
 
 
-class InvalidResource(InternalError):
+class UnknownErrorCode(JSONRPCException):
+    code = -32001
+    message = ""
+
+
+class RequestFailed(JSONRPCException):
+    """A request failed but it was syntactically correct"""
+
+    code = -32803
+    message = "request failed"
+
+
+class ServerCancelled(JSONRPCException):
+    """The server cancelled the request"""
+
+    code = -32802
+    message = "server cancelled"
+
+
+class ContentModified(JSONRPCException):
+    """The content of a document got modified outside normal conditions"""
+
+    code = -32801
+    message = "content modified"
+
+
+class RequestCancelled(JSONRPCException):
+    """The client has canceled a request and a server has detected the cancel"""
+
+    code = -32800
+    message = "request cancelled"
+
+
+class ServerError(JSONRPCException):
+    """Reserved for implementation-defined server-errors
+
+    Valid error code from  -32099 to -32000.
+    """
+
+    message: str
+    code: int
+
+    def __init__(self, *args: object) -> None:
+        min_range = -32099
+        max_range = -32000
+        if not (min_range < self.code < max_range):
+            raise ValueError(f"valid code {min_range} to {max_range}")
+        super().__init__(*args)
+
+
+class InvalidResource(ServerError):
     """invalid resource"""
 
     message = "invalid resource"
+    code = -32001
 
 
-class RequestCancelled(RPCException):
-    """request canceled"""
-
-    message = "request canceled"
-    code = -32800
-
-
-class ContentModified(RPCException):
-    """content modified"""
-
-    message = "content modified"
-    code = -32801
-
-
-class FeatureDisabled(InternalError):
+class FeatureDisabled(ServerError):
     """feature disabled"""
 
     message = "feature disabled"
+    code = -32002
 
 
 def transform_error(
-    error: Union[RPCException, Exception, None]
+    error: Union[JSONRPCException, Exception, None],
 ) -> Optional[Dict[str, Any]]:
     """transform exception to rpc error"""
 
     if not error:
         return None
 
-    code, message = None, None
     try:
         code = error.code
-        message = str(error) or error.message
+        message = error.message
 
     except AttributeError:
         # 'err.code' and 'err.message' may be not defined
