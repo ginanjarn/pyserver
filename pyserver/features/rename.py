95b8161f78466c2023307d9d455ee61609f51558
ginanjarn <ginanjarn@gmail.com>
2026-04-15 11:55:57 +0700

    improve build rename result

--- pyserver/features/rename.py
+++ pyserver/features/rename.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import Dict, Any
 
 from jedi import Script, Project
-from jedi.api.refactoring import Refactoring, RefactoringError
+from jedi.api.refactoring import ChangedFile, Refactoring, RefactoringError
 
 from pyserver import errors
 from pyserver.uri import uri_to_path, path_to_uri
@@ -44,45 +44,50 @@ class RenameProvider:
         except RefactoringError as err:
             raise errors.InvalidRequest(repr(err)) from err
 
-    def build_item(self, refactor: Refactoring):
-        for file_path, change in refactor.get_changed_files().items():
-            # File Resource Changes
-            old = change._from_path
-            new = change._to_path
-            if old != new:
-                yield {
-                    "kind": "rename",
-                    "oldUri": path_to_uri(old),
-                    "newUri": path_to_uri(new),
-                }
-                continue
 
-            # TextEdit changes
-
-            # 'new_text' from jedi has variance on newline.
-            # Normalize newline to '\n'.
-            new_text = change.get_new_code()
-            new_text = new_text.replace("\r\n", "\n").replace("\r", "\n")
-
-            try:
-                document = self.params.session.get_document(file_path)
-            except errors.InvalidResource:
-                temp_text = file_path.read_text()
-                document = Document(
-                    self.params.workspace_path, file_path, "", 0, temp_text
-                )
-
-            yield {
-                "textDocument": {
-                    "version": document.version,
-                    "uri": path_to_uri(document.file_path),
-                },
-                "edits": diffutils.get_text_changes(document.text, new_text),
+    def build_item(self, path: Path, changed_file: ChangedFile) -> Dict[str, Any]:
+        # File Resource Changes
+        old = changed_file._from_path
+        new = changed_file._to_path
+        if old != new:
+            return {
+                "kind": "rename",
+                "oldUri": path_to_uri(old),
+                "newUri": path_to_uri(new),
             }
+
+        # TextEdit changes
+
+        # 'new_text' from jedi has variance on newline.
+        # Normalize newline to '\n'.
+        new_text = changed_file.get_new_code()
+        new_text = new_text.replace("\r\n", "\n").replace("\r", "\n")
+
+        try:
+            document = self.params.session.get_document(path)
+        except errors.InvalidResource:
+            temp_text = path.read_text()
+            document = Document(self.params.workspace_path, path, "", 0, temp_text)
+
+        return {
+            "textDocument": {
+                "version": document.version,
+                "uri": path_to_uri(document.file_path),
+            },
+            "edits": diffutils.get_text_changes(document.text, new_text),
+        }
 
     def get_changes(self) -> Dict[str, Any]:
         refactored = self.execute()
-        return {"documentChanges": list(self.build_item(refactored))}
+        changed_files = refactored.get_changed_files()
+        if not changed_files:
+            return None
+
+        changes = [
+            self.build_item(path, changed_file)
+            for path, changed_file in changed_files.items()
+        ]
+        return {"documentChanges": changes}
 
 
 def textdocument_rename(session: Session, params: dict) -> None:
